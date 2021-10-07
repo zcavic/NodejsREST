adc956b113296290ee573062d1af5b5a23e5d73e
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-07 22:52:52 +0200

    Intorduce templates

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const app = express();
 app.use(morgen('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.send('Hello from my app.');
+    res.render('index', {title: ' home.'});
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     debug(`listening on port ${chalk.green(PORT)}`);
 })
