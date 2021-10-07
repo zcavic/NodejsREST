cb28f5e2f5f6df42b430388b98a6a6113245021e
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-07 18:08:07 +0200

    Serving static file

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgen = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgen('tiny'));
-
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('Hello from my app.');
