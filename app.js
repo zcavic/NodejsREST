93017936e172df2a9b90a276556af610f42a7b0f
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-07 22:56:55 +0200

    Add data from list to template

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index', {title: ' home.'});
+    res.render('index', {title: ' home.', data: ['a', 'b', 'c']});
 })
 
 app.listen(PORT, ()=>{
