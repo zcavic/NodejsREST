d04bcf8d599d2a14bfbe9d238f2f09669c6d3290
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-09 11:49:28 +0200

    Get sesion from mongodb

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const debug = require('debug')('app:sessionsRouter');
-const { MongoClient } = require('mongodb');
-const sessions = require('../data/sessions.json');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const sessionsRouter = express.Router();
 
@@ -25,9 +24,20 @@ sessionsRouter.route('/').get((req, res) => {
 
 sessionsRouter.route('/:id').get((req, res) => {
   const id = req.params.id;
-  res.render('session', {
-    session: sessions[id],
-  });
+  const url = 'mongodb+srv://dbUser:4wvTkSQovddkQvwd@cluster0.b2tlh.mongodb.net?retryWrites=true&w=majority';
+  const dbName = 'globomantics';
+  (async function mongo(){
+      let client;
+      try{
+          client = await MongoClient.connect(url);
+          debug('Connected to the database');
+          const db = client.db(dbName);
+          const session = await db.collection('sessions').findOne({ _id: new ObjectId(id) });
+          res.render('session', { session });
+      } catch(error ){
+          debug(error.stack);
+      }
+  }())
 });
 
 module.exports = sessionsRouter;
