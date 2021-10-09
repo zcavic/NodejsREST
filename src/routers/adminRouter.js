090f56f5cf1160e9f7edf37d767ed4fdd1b18fc9
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-09 10:53:50 +0200

    Admin router and mongodb

---
+++ src/routers/adminRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const debug = require('debug')('app:adminRouter');
+const { MongoClient } = require('mongodb');
+const sessions = require('../data/sessions.json');
+
+const adminRouter = express.Router();
+
+adminRouter.route('/').get((req, res)=>{
+    const url = 'mongodb+srv://dbUser:4wvTkSQovddkQvwd@cluster0.b2tlh.mongodb.net?retryWrites=true&w=majority';
+    const dbName = 'globomantics';
+
+    (async function mongo(){
+        let client;
+        try{
+            client = await MongoClient.connect(url);
+            debug('Connected to the database');
+            const db = client.db(dbName);
+            const response = await db.collection('session').insertMany(sessions);
+            res.json = (response);
+        } catch(error ){
+            debug(error.stack);
+        }
+    }())
+})
+
+module.exports = adminRouter;
