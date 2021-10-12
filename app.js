1fa1d1e9af56b3e511c81db7e1e49293ae6cc506
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-12 20:03:58 +0200

    Passport for authentication

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgen = require('morgan');
 const path = require('path');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,10 +13,16 @@ const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 const authRouter = require('./src/routers/authRouter');
 
+// TODO order of execution 'configure passport Module 8 4:55'
+// TODO is this controller
 app.use(morgen('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use(express.json()); // what is this?
+app.use(express.json()); // TODO
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser);
+app.use(session({secret: 'globomantics'}));
+
+require('./src/config/passport.js')(app); // TODO
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
