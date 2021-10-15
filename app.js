3102ca4349e6797b0ee25531f08285a45de78dfa
Zeljko Cavic <zeljkochavic@gmail.com>
2021-10-15 21:48:20 +0200

    Added comments in for tracing the problem in AWS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+debug('Require process is started.');
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
@@ -6,13 +7,14 @@ const path = require('path');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 const authRouter = require('./src/routers/authRouter');
+debug('Require process is finished.');
 
+debug('Using process is started.');
 // TODO order of execution 'configure passport Module 8 4:55'
 // TODO is this controller
 app.use(morgen('tiny'));
@@ -26,10 +28,10 @@ require('./src/config/passport.js')(app); // TODO this is related with the order
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
 app.use('/sessions', sessionsRouter);
 app.use('/admin', adminRouter);
 app.use('/auth', authRouter);
+debug('Using process is finished.');
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Globomantics.', data: ['a', 'b', 'c']});
